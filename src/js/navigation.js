09695bfffe8a7f22f1a107587ebdd991ef9ce57b
JC Delay <delay@adacore.com>
2013-11-18 18:31:52 +0100

    Fix Firefox issue.
    window.location.origin returns null. Use the following instead:
    
        window.location.protocol + '//'
    
    Change-Id: I2d4e2d38802e279997eb0a1cc1d072778ba65355

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -114,7 +114,9 @@ xcov.navigation.eventTarget = new goog.events.EventTarget();
  */
 xcov.navigation.initialize = function(opt_window) {
   /** @const */ var win = opt_window || window;
-  xcov.navigation.baseURL = win.location.origin + win.location.pathname;
+
+  xcov.navigation.baseURL = goog.string.buildString(win.location.protocol, '//',
+      win.location.pathname);
 
   goog.events.listen(xcov.navigation.history_, goog.history.EventType.NAVIGATE,
       xcov.navigation.onNavigate_);
