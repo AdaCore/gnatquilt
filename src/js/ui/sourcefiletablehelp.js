f31d9774b0b10867c037c0ee5d4a76146f78e342
Charly Delay <delay@adacore.com>
2016-09-16 09:55:02 -0400

    doc: add mention of the --not-coverable mode
    Change-Id: I3dc7541efdd35cf2f6d809193f0dd22520a263b5
    TN: P909-023

--- src/js/ui/sourcefiletablehelp.js
+++ src/js/ui/sourcefiletablehelp.js
@@ -64,7 +64,10 @@ xcov.ui.SourceFileTableHelp.prototype.createDom = function() {
           'criteria;'),
       dom.createDom(goog.dom.TagName.LI, null,
           'the number of such lines that are part of an exemption ' +
-          'region, with or without actually exempted violations'),
+          'region, with or without actually exempted violations;'),
+      dom.createDom(goog.dom.TagName.LI, null,
+          'the number of such lines that are not coverable because no' +
+          'machine code is generated (if requested by --not-coverable);'),
       dom.createDom(goog.dom.TagName.LI, null,
           'a visual summary of this coverage data.'));
 
