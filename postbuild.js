393305c43024865056400c6564290167e23d0498
Matthieu Eyraud <eyraud@adacore.com>
2022-05-05 14:45:53 +0200

    Fix highlighting in source file view
    Source code highlighting never worked in production, as it only worked
    with a server.
    
    Change-Id: I777901c8c654fd495aceb55302d28f4a9a0919e7
    TN: R417-010

--- postbuild.js
+++ postbuild.js
@@ -20,6 +20,13 @@ fs.readFile(indexFilePath, 'utf8', function (err, data) {
   // a server).
   $('html').find('script').removeAttr('type');
 
+  // Yet another hack: as we are generating a static html page, we can't load scripts through HTTP request. Highligh.js
+  // relies on such a mechanism to load its scripts. To work around that, we will load the generated scripts
+  // preemptively, so that they are not loaded later through HTTP requests.
+  $('html').append('<script src=167.js></script>');
+  $('html').append('<script src=343.js></script>');
+  $('html').append('<script src=700.js></script>');
+
   fs.writeFile(indexFilePath, $.html(), function (err) {
     if (err) return console.log(err);
     console.log('Successfully rewrote index html');
