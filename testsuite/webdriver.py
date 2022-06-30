590b272746eb848894393a344c17c9178837995e
Matthieu Eyraud <eyraud@adacore.com>
2022-06-30 18:16:53 +0200

    gnatquilt testsuite: add utilities to test subprogram metrics
    This commits adds the following utilities:
      * Check a particular statistic for a given subprogram
      * Navigate to the subprogram source using the navigation links
    
    It also make the testsuite adaptations (accounting for changes in the
    DOM), required in gnatquilt after the change implementing support for
    subprogram metrics.
    
    Change-Id: Ic6e9b3e0906834f4ccfbcf093cabc4340b9970f9
    TN: R417-010

--- testsuite/webdriver.py
+++ testsuite/webdriver.py
@@ -29,9 +29,7 @@ class FirefoxDriver(webdriver.Firefox):
 
     def __column_number_for_status(self, status):
         status_index = 0
-        columns = self.find_elements(
-            By.XPATH, "//td[contains(.,'Total')]/ancestor::table//th"
-        )
+        columns = self.find_elements(By.XPATH, "//app-enumerable-table//th")
         for column in columns:
             if column.text == status:
                 break
@@ -103,7 +101,7 @@ class FirefoxDriver(webdriver.Firefox):
             1
         ]
         project_stats = project_table.find_elements(
-            By.XPATH, f".//div[text()='{project}']/ancestor::tr//td"
+            By.XPATH, f".//span[text()='{project}']/ancestor::tr//td"
         )
         assert int(project_stats[status_index].text.split()[0]) == statistic
 
@@ -114,6 +112,68 @@ class FirefoxDriver(webdriver.Firefox):
         status_index = self.__column_number_for_status(str(status))
         total_table = self.find_elements(By.XPATH, "//app-enumerable-table")[0]
         total_stats = total_table.find_elements(
-            By.XPATH, ".//div[text()='Total']/ancestor::tr//td"
+            By.XPATH, ".//span[text()='Total']/ancestor::tr//td"
         )
         assert int(total_stats[status_index].text.split()[0]) == statistic
+
+    def find_subp_tr(self, subp_name):
+        """
+        :param string subp_name: name of the subprogram for which we want to
+        find the corresponding row in the source header.
+        :return WebElement: corresponding row HTML element.
+
+        Note that this function assumes no duplicates for subprogram names
+        (good enough for our testing purposes).
+        """
+        subp_tr = []
+        expanded = True
+        while not subp_tr and expanded:
+            expanded = False
+            subp_tr = self.find_elements(
+                By.XPATH,
+                f"//app-enumerable-table//span[text()='{subp_name}']"
+                "/ancestor::tr",
+            )
+            tds_filename = self.find_elements(
+                By.CLASS_NAME, "xcov-table-filename"
+            )
+            for td_filename in tds_filename:
+                expand = td_filename.find_elements(
+                    By.XPATH, ".//mat-icon[text() =' expand_more ']"
+                )
+                if expand:
+                    expand[0].click()
+                    expanded = True
+
+        # Check that we have found one (and only one) match.
+        assert subp_tr and len(subp_tr) == 1
+        return subp_tr[0]
+
+    def check_subp_stat(self, subp_name, status, statistic):
+        """
+        Same as check_stats_for_file, but checks subprogram statistics. Only
+        works if the user navigated to the file in which the procedure body is
+        located (where the subprogram metrics are reported).
+
+        Expand nested metrics as long as it did not find a match. Note that
+        this function assumes no duplicates (good enough for our testing
+        purposes).
+        """
+        subp_tr = self.find_subp_tr(subp_name)
+        subp_tds = subp_tr.find_elements(By.XPATH, ".//td")
+
+        status_index = self.__column_number_for_status(str(status))
+        assert int(subp_tds[status_index].text.split()[0]) == statistic
+
+    def navigate_to_subp(self, subp_name):
+        """
+        :param string subp_name: name of the subprogram.
+
+        Navigate to the given subprogram by clicking on it in the header.
+        """
+        subp_tr = self.find_subp_tr(subp_name)
+        (
+            subp_tr.find_element(By.CLASS_NAME, "xcov-table-filename")
+            .find_element(By.XPATH, ".//span")
+            .click()
+        )
